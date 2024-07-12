55063c0b71e9457548666806e95d1f096f180ce8
Anirudh-1606 <anirudhsaladi7@gmail.com>
2024-07-12 16:04:52 +0530

    updated import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SideNav from "@/components/dashboard/sidenav";
+import SideNav from "@/components/common/sidenav";
 
 const inter = Inter({ subsets: ["latin"] });
 
